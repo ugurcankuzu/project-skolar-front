fbcb34caa27be2960014b2f16adc86fa5ce1be77
Uğurcan KUZU <uci2001@hotmail.com>
2025-08-08 16:21:19 +0300

    Topic route changed

--- src/helpers/getTopics.ts
+++ src/helpers/getTopics.ts
@@ -1,5 +1,5 @@
 export default async function getTopics(id: number) {
-  const res = await fetch("/api/classes/" + id + "/topics");
+  const res = await fetch("/api/topics/" + id);
   const data = await res.json();
   return data;
 }
